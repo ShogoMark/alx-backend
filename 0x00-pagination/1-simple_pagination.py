910af55d51c4027ec309b2377cf3e0e6f0fda1fd
ShogoMark <trigmark87@gmail.com>
2023-07-23 14:28:09 +0100

    task1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -35,11 +35,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """function takes in page & page_size, returns a list"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
         dataset = self.dataset()
 
-        index_range(page, page_size)
+        start_index, end_index = index_range(page, page_size)
 
         return dataset[start_index:end_index]

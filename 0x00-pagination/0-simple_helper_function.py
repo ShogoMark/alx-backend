23a911dad6dd495858c797d7e7ef5c42ec6e0d1d
ShogoMark <trigmark87@gmail.com>
2023-07-23 11:38:14 +0100

    read

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""function that returns a tuple of index of page"""
+
 
 def index_range(page: int, page_size: int) -> tuple:
+    """function takes in args page & page size"""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     new_tuple = (start_index, end_index)

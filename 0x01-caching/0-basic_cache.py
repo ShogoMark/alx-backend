64b99d5d22ee01fd09ca274008bd7c7bd31badc4
ShogoMark <trigmark87@gmail.com>
2023-07-25 20:34:52 +0100

    task0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
+"""A caching module with class BasicCache"""
 
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """inherits from parent class base_caching"""
+
+    # function that assigns the item to the key
     def put(self, key, item):
-        if key == None or item == None:
+        """takes in argument key and item"""
+        if key is None or item is None:
             pass
         else:
             self.cache_data[key] = item
 
+    # function to retrieve the item of the key argument
     def get(self, key):
+        """takes in argument key"""
         for keys in self.cache_data:
             if keys == key:
                 return self.cache_data[key]

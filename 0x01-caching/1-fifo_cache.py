14bc57a1af96d7b8aac676dc4782262951e2b89a
ShogoMark <trigmark87@gmail.com>
2023-07-27 02:12:55 +0100

    task1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
 """A FIFOCache that inherits from BaseCaching"""
 
-from base_caching import BaseCaching:
+from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class FIFOCache(BaseCaching):
     """class inherits from BaseCaching"""
     def __init__(self):
         super().__init__()
+        self.cache_data = OrderedDict()
 
 
     def put(self, key, item):
         """functions takes in key and item"""
         if key is None or item is None:
             return
-
-        if len(self.cache_data) > self.MAX_ITEMS:
-            if key not in self.cache_data:
-                oldest_key = next(iter(self.cache_data)) # Get the oldest key
-                del self.cache_data[oldest_key]
-                print("DISCARD: {}".format(oldest_key))
-            else:
-                return self.cache_data
-         
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
 
     def get(self, key):
         """function takes in key as argument"""

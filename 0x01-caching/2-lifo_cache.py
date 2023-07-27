dc1f5e4d34daf9d08af7e3f91c2c1c4af34841b0
ShogoMark <trigmark87@gmail.com>
2023-07-27 02:50:25 +0100

    task2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,28 +1,31 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """A LIFOCache that inherits from BaseCaching"""
 
 from base_caching import BaseCaching
+from collections import OrderedDict
+
 
 class LIFOCache(BaseCaching):
     """class inherits from BaseCaching"""
     def __init__(self):
+        """initialize the function"""
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """functions takes in key and item"""
         if key is None or item is None:
-            pass
-
-        if len(self.cache_data) >= self.MAX_ITEMS:
-            old_key = next(reversed(self.cache_data))
-            del self.cache_data[old_key]
-            print("DISCARD: {}".format(old_key))
-
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > self.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
         self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
         """function takes in key as argument"""
-        for keys in self.cache_data:
+        for keys in key:
             if key == keys:
                 return self.cache_data[key]
         return None

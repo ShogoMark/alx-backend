48996b09de50f9dc0bb130ddaddca4cfe0d6d779
ShogoMark <trigmark87@gmail.com>
2023-08-01 17:59:07 +0100

    task2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""A flask app"""
+
+from flask import Flask, render_template
+from flask.wrappers import Response
+from flask_babel import Babel
+
+app: Flask = Flask(__name__)
+
+
+class Config:
+    """Class config to instantiate attributes"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    sup_lang = app.config['LANGUAGES']
+    return request.accept_languages.best_match(sup_lang)
+
+
+@app.route('/')
+def index() -> str:
+    """index function returning an html page"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

986782523742bff34077b3859ccefd1e86d569c4
ShogoMark <trigmark87@gmail.com>
2023-08-01 15:47:29 +0100

    task0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A flask app"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """index function returning an html page"""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

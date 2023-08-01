eef85787d0b8fd64d8a0c31b8647e5b7797790bd
ShogoMark <trigmark87@gmail.com>
2023-08-01 17:40:23 +0100

    task1s

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,12 +6,21 @@ from flask.wrappers import Response
 from flask_babel import Babel
 
 app: Flask = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+
 babel = Babel(app)
 
 @app.route('/')
 def index() -> str:
     """index function returning an html page"""
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':

93472bea50dbe14ac4eeba6d473624bc8981e798
ShogoMark <trigmark87@gmail.com>
2023-08-01 15:58:30 +0100

    task0s

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,15 +2,15 @@
 """A flask app"""
 
 from flask import Flask, render_template
+from flask.wrappers import Response
 
-
-app = Flask(__name__)
+app: Flask = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index() -> str:
     """index function returning an html page"""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':

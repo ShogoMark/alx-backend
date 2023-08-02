3c6a51065ac904cb4cb06f746ab561b2f17396d6
ShogoMark <trigmark87@gmail.com>
2023-08-02 06:53:44 +0100

    task0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,11 +2,11 @@
 """A flask app"""
 
 from flask import Flask, render_template
-from flask.wrappers import Response
 from flask_babel import Babel
 
 app: Flask = Flask(__name__)
 babel = Babel(app)
+
 
 @app.route('/')
 def index() -> str:
